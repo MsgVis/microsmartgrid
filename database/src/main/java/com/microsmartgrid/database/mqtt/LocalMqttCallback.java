9b6a2c7bb15994b26b9d59586cf228476155fb40
dustin <dustin@weeseweb.de>
2019-11-25 22:58:35 +0100

    refactor: Explain intended logic, reuse createClassFromString
    The intended use of the createFromString class was too simple, so I reused it for deserializing the json from mqtt into an object.
    This class will probably be broken up into more concise classes when it becomes too big.

--- database/src/main/java/com/microsmartgrid/database/mqtt/LocalMqttCallback.java
+++ database/src/main/java/com/microsmartgrid/database/mqtt/LocalMqttCallback.java
@@ -1,23 +1,40 @@
 package com.microsmartgrid.database.mqtt;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
+import com.microsmartgrid.database.dbDataStructures.AbstractDevice;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.lang.reflect.Type;
+
 public class LocalMqttCallback implements MqttCallback {
 	private static final Logger logger = LogManager.getLogger(LocalMqttCallback.class.getName());
 
 	/**
-	 * As explained here: https://stackoverflow.com/questions/1268817/create-new-object-from-a-string-in-java
-	 * No checking for
-	 *
-	 * @param class_name - the name of the
-	 * @throws ClassNotFoundException
+	 * @param json - Json to be deserialized
+	 * @param class_name - Snippet of topic for which class names are defined
 	 */
-	public void createClassFromString(String class_name) throws ClassNotFoundException {
-		//TODO
+	public <T extends AbstractDevice> T createObjectFromJson(String json, String class_name) throws JsonParseException {
+		// TODO: configure gson
+		Gson gson = new Gson();
+		Class<?> clazz;
+		try {
+			clazz = Class.forName(class_name);
+		} catch(ClassNotFoundException ex) {
+			// log error
+			// use default class to save fields in 'meta_information' and create an 'id', 'timestamp', and a corresponding 'DeviceInformation'
+			clazz = AbstractDevice.class;
+		}
+
+		// TODO: check 'Device_Information' for existing 'device' and 'name' and create 'DeviceInformation' object for new devices
+
+		// create object from json TODO: handle unchecked cast
+		return gson.fromJson(json, (Class<T>) clazz);
 	}
 
 	@Override
@@ -28,8 +45,9 @@ public class LocalMqttCallback implements MqttCallback {
 	@Override
 	public void messageArrived(String topic_name, MqttMessage mqttMessage) throws Exception {
 		try {
-			createClassFromString(topic_name);
-		} catch (Exception e) { //TODO appropriate choice of exceptions
+			// TODO: split topic_name to into parts so we can determine the appropriate class to deserialize to
+			createObjectFromJson(mqttMessage.toString(), topic_name);
+		} catch (JsonParseException e) {
 			//TODO logging
 		}
 	}
