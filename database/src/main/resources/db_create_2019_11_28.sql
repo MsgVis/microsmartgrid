237b97cb300606ad32b81e9485d62a41b8e0c8bc
dustin <dustin@weeseweb.de>
2019-11-28 22:47:04 +0100

    chore: Add schema with following changes:
    Id generation on devices with SERIAL
    Add timescaledb extension
    Add not nullable constraint on device name to make sure its possible to assign devices
    Renamed id in devices to avoid confusion and eliminate repetition

---
+++ database/src/main/resources/db_create_2019_11_28.sql
@@ -0,0 +1,48 @@
+CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
+
+CREATE TABLE devices (
+	id	SERIAL	PRIMARY KEY,
+	name		TEXT	NOT NULL,
+	description	TEXT,
+	type		TEXT,
+	subtype		TEXT,
+	depth		INTEGER,
+	icon		TEXT,
+	children	INTEGER[]
+);
+
+CREATE TABLE readings (
+	time		TIMESTAMP		,
+  device_id	INTEGER	REFERENCES devices (id),
+	a_minus		REAL			,
+	a_plus		REAL			,
+	r_minus 	REAL			,
+	r_plus 		REAL			,
+	p_total 	REAL			,
+	p_r		REAL			,
+	p_s		REAL			,
+	p_t		REAL			,
+	q_total		REAL			,
+	q_r		REAL			,
+	q_s		REAL			,
+	q_t		REAL			,
+	s_total		REAL			,
+	s_r		REAL			,
+	s_s		REAL			,
+	s_t		REAL			,
+	i_avg		REAL			,
+	i_r		REAL			,
+	i_s		REAL			,
+	i_t		REAL			,
+	u_avg		REAL			,
+	u_r		REAL			,
+	u_s		REAL			,
+	u_t		REAL			,
+	f		REAL			,
+	meta		JSON			,
+	PRIMARY KEY (time,device_id)
+);
+
+SELECT create_hypertable('readings', 'time');
+
+CREATE INDEX ON readings (device_id, time DESC);
