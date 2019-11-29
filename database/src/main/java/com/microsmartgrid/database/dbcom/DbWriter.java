71a7fc61f28ea6b37bb41b50238b6505559d0147
dustin <19474420+Bytesizeweezy@users.noreply.github.com>
2019-11-29 11:34:01 +0100

    refactor: Move objectMapper to it's own class, extend skeleton

--- database/src/main/java/com/microsmartgrid/database/dbcom/DbWriter.java
+++ database/src/main/java/com/microsmartgrid/database/dbcom/DbWriter.java
@@ -2,32 +2,37 @@ package com.microsmartgrid.database.dbcom;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsmartgrid.database.ObjectMapperManager;
 import com.microsmartgrid.database.dbDataStructures.AbstractDevice;
 
 public class DbWriter {
 
 	public static <T extends AbstractDevice> void deserializeJson(String json, String topic, Class clazz) throws JsonProcessingException {
-		// TODO: configure objectMapper
-		ObjectMapper objMapper = new ObjectMapper();
-		objMapper.addMixIn(clazz, objMapper.findMixInClassFor(clazz));
+		ObjectMapper objMapper = ObjectMapperManager.getObjectMapper();
 
 		// TODO: infer Class (at least <T extends AbstractDevice>)
 		Object device = null;
 		Object deviceInfo = null;
 
 		// TODO: check 'Device_Information' for existing 'device' and 'name' and create 'DeviceInformation' object for new devices
-		DbReader.getDeviceInfo(topic);
+		deviceInfo = DbReader.getDeviceInfo(topic);
 
 		if(objMapper.canSerialize(clazz)){
 			// create object from json
 			device = objMapper.readValue(json, clazz);
+		} else {
+			// not a json, throw an exception for now
+			throw new UnsupportedOperationException("Input is not a json.");
 		}
 
-		writeToDatabase(deviceInfo, device);
+		writeDeviceToDatabase(deviceInfo, device);
 	}
 
-	private static void writeToDatabase(Object deviceInfo, Object device) {
+	private static void writeDeviceToDatabase(Object deviceInfo, Object device) {
+		// if deviceInfo null, create it first
+		// (note: new object probably needs to be updated (flushed) to be able to retrieve generated id)
 
+		// assign id from deviceInfo to device
 	}
 
 
