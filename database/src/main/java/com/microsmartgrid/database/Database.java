bb25abf06b7622e4cd02adc11cc2f171c78bd2a0
dustin <19474420+Bytesizeweezy@users.noreply.github.com>
2019-12-11 11:39:32 +0100

    fix: Method not being recognized as a main
    If there are more parameters than args, even if optional, it wont be recognized as the main method to run on execution.
    
    Use a specific topic with mqtt.eclipse.org so the output isn't a bunch of expected errors that it can't deserialize the content.

--- database/src/main/java/com/microsmartgrid/database/Database.java
+++ database/src/main/java/com/microsmartgrid/database/Database.java
@@ -10,24 +10,24 @@ public class Database {
 	private static final Logger logger = LogManager.getLogger(Database.class.getName());
 	private static String msg_serverURI;
 	private static String mqtt_topic;
+	private static String mqtt_timeout;
 
 	/**
 	 * @param args - first: serverURI, second: topic
 	 */
-	public static void main(String[] args, int... mqtt_timeout) {
+	public static void main(String[] args) {
 		if (args.length == 0 || args[0].isEmpty()) msg_serverURI = "tcp://192.168.121.172";
 		else msg_serverURI = args[0];
 		if (args.length < 2 || args[1].isBlank()) mqtt_topic = "#";
 		else mqtt_topic = args[1];
+		if (args.length < 3 || args[2].isBlank()) {
+			logger.info("No connection timeout was specified. Connecting with timeout 30.");
+			mqtt_timeout = "30";
+		} else mqtt_timeout = args[2];
 
 		LocalMqttAsyncClient mqtt_client = new LocalMqttAsyncClient();
 		mqtt_client.init(msg_serverURI);
-		if (mqtt_timeout.length > 0) mqtt_client.connect(mqtt_timeout[0]);
-		else {
-			logger.info("No connection timeout was specified. Connecting with timeout 30.");
-			mqtt_client.connect(30);
-		}
-		mqtt_client.connect(mqtt_timeout.length > 0 ? mqtt_timeout[0] : 30);
+		mqtt_client.connect(Integer.parseInt(mqtt_timeout));
 		mqtt_client.standardSubscribe(mqtt_topic);
 
 	}
