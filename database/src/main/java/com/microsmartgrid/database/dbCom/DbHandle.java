be591a028ad01c856cb5b89644166722002798bf
Lucca Johannes Gutleben <lucca.j.gutleben@campus.tu-berlin.de>
2019-12-13 16:43:51 +0100

    fix: DbHandleTest & DbWriterTest

--- database/src/main/java/com/microsmartgrid/database/dbCom/DbHandle.java
+++ database/src/main/java/com/microsmartgrid/database/dbCom/DbHandle.java
@@ -14,8 +14,8 @@ public class DbHandle {
 
 	private static final Logger logger = LogManager.getLogger(DbHandle.class.getName());
 	private static final String INSERT_READINGS_SQL = "INSERT INTO " +
-		"readings (time, device_id, a_minus, a_plus, r_minus, r_plus, p_total, p_r, p_s, p_t, q_total, q_r, q_s, q_t, s_total, s_r, s_s, s_t, i_avg, i_r, i_s, i_t, u_avg, u_r, u_s, u_t, f, meta" +
-		"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, DEFAULT);";
+		"readings (time, device_id, a_minus, a_plus, r_minus, r_plus, p_total, p_r, p_s, p_t, q_total, q_r, q_s, q_t, s_total, s_r, s_s, s_t, i_avg, i_r, i_s, i_t, u_avg, u_r, u_s, u_t, f, meta) " +
+		"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, DEFAULT);";
 	private static final String INSERT_DEVICES_SQL = "INSERT INTO " +
 		"devices (id, name, description, type, subtype, children) " +
 		"VALUES (DEFAULT, ?, ?, ?, ?, ?);";
@@ -49,8 +49,8 @@ public class DbHandle {
 
 			info.setString(1, deviceInfo.getName());
 			info.setString(2, deviceInfo.getDescription());
-			info.setString(3, deviceInfo.getType().toString());
-			info.setString(4, deviceInfo.getSubtype().toString());
+			info.setString(3, deviceInfo.getType() == null ? null : deviceInfo.getType().toString());
+			info.setString(4, deviceInfo.getSubtype() == null ? null : deviceInfo.getSubtype().toString());
 			info.setArray(5, conn.createArrayOf("INTEGER", deviceInfo.getChildren()));
 
 			if (info.executeUpdate() > 0) {
@@ -86,27 +86,26 @@ public class DbHandle {
 			reading.setFloat(5, device.getReactive_energy_R_minus());
 			reading.setFloat(6, device.getReactive_energy_R_plus());
 			reading.setFloat(7, device.getActive_power_P_total());
-			reading.setFloat(8, device.getActive_power_P_total());
-			reading.setFloat(9, device.getActive_power_P1());
-			reading.setFloat(10, device.getActive_power_P2());
-			reading.setFloat(11, device.getActive_power_P3());
-			reading.setFloat(12, device.getReactive_power_Q_total());
-			reading.setFloat(13, device.getReactive_power_Q1());
-			reading.setFloat(14, device.getReactive_power_Q2());
-			reading.setFloat(15, device.getReactive_power_Q3());
-			reading.setFloat(16, device.getApparent_power_S_total());
-			reading.setFloat(17, device.getApparent_power_S1());
-			reading.setFloat(18, device.getApparent_power_S2());
-			reading.setFloat(19, device.getApparent_power_S3());
-			reading.setFloat(20, device.getCurrent_I_avg());
-			reading.setFloat(21, device.getCurrent_I1());
-			reading.setFloat(22, device.getCurrent_I2());
-			reading.setFloat(23, device.getCurrent_I3());
-			reading.setFloat(24, device.getVoltage_U_avg());
-			reading.setFloat(25, device.getVoltage_U1());
-			reading.setFloat(26, device.getVoltage_U2());
-			reading.setFloat(27, device.getVoltage_U3());
-			reading.setFloat(28, device.getFrequency_grid());
+			reading.setFloat(8, device.getActive_power_P1());
+			reading.setFloat(9, device.getActive_power_P2());
+			reading.setFloat(10, device.getActive_power_P3());
+			reading.setFloat(11, device.getReactive_power_Q_total());
+			reading.setFloat(12, device.getReactive_power_Q1());
+			reading.setFloat(13, device.getReactive_power_Q2());
+			reading.setFloat(14, device.getReactive_power_Q3());
+			reading.setFloat(15, device.getApparent_power_S_total());
+			reading.setFloat(16, device.getApparent_power_S1());
+			reading.setFloat(17, device.getApparent_power_S2());
+			reading.setFloat(18, device.getApparent_power_S3());
+			reading.setFloat(19, device.getCurrent_I_avg());
+			reading.setFloat(20, device.getCurrent_I1());
+			reading.setFloat(21, device.getCurrent_I2());
+			reading.setFloat(22, device.getCurrent_I3());
+			reading.setFloat(23, device.getVoltage_U_avg());
+			reading.setFloat(24, device.getVoltage_U1());
+			reading.setFloat(25, device.getVoltage_U2());
+			reading.setFloat(26, device.getVoltage_U3());
+			reading.setFloat(27, device.getFrequency_grid());
 
 			reading.executeUpdate();
 			conn.commit();
--- database/src/test/java/com/microsmartgrid/database/dbCom/DbWriterTest.java
+++ database/src/test/java/com/microsmartgrid/database/dbCom/DbWriterTest.java
@@ -6,14 +6,14 @@ import com.microsmartgrid.database.ObjectMapperManager;
 import com.microsmartgrid.database.dbDataStructures.AbstractDevice;
 import com.microsmartgrid.database.dbDataStructures.AdditionalDeviceInformation;
 import com.microsmartgrid.database.dbDataStructures.DaiSmartGrid.Battery;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 
 public class DbWriterTest {
 
-	@BeforeAll
+	@AfterAll
 	static void cleanUp() {
 		DbHandle db = new DbHandle();
 		db.execute("DROP ALL OBJECTS;");
@@ -43,6 +43,60 @@ public class DbWriterTest {
 		info.setSubtype(AdditionalDeviceInformation.Subtype.BATTERY);
 		info.setChildren(new Integer[]{1, 2});
 		db.insertDeviceInfo(info);
+
+		db.execute(
+			"CREATE TABLE readings (" +
+				"time TIMESTAMP," +
+				"device_id INTEGER," +
+				"a_minus REAL," +
+				"a_plus REAL," +
+				"r_minus REAL," +
+				"r_plus REAL," +
+				"p_total REAL," +
+				"p_r REAL," +
+				"p_s REAL," +
+				"p_t REAL," +
+				"q_total REAL," +
+				"q_r REAL," +
+				"q_s REAL," +
+				"q_t REAL," +
+				"s_total REAL," +
+				"s_r REAL," +
+				"s_s REAL," +
+				"s_t REAL," +
+				"i_avg REAL," +
+				"i_r REAL," +
+				"i_s REAL," +
+				"i_t REAL," +
+				"u_avg REAL," +
+				"u_r REAL," +
+				"u_s REAL," +
+				"u_t REAL," +
+				"f REAL," +
+				"meta JSON" +
+				");");
+
+		DbWriter.deserializeJson(json, "battery", Class.forName("com.microsmartgrid.database.dbDataStructures.DaiSmartGrid.Battery").asSubclass(AbstractDevice.class));
+	}
+
+	@Test
+	void testDeserializeJsonWithoutDeviceInfo() throws ClassNotFoundException, JsonProcessingException {
+		Battery bat = new Battery((float) 0.0);
+		bat.setTimestamp(Instant.now());
+
+		ObjectMapper objMapper = ObjectMapperManager.getMapper();
+		String json = objMapper.writeValueAsString(bat);
+
+		DbHandle db = new DbHandle();
+		db.execute(
+			"CREATE TABLE devices (" +
+				"id INTEGER auto_increment," +
+				"name TEXT," +
+				"description TEXT," +
+				"type TEXT," +
+				"subtype TEXT," +
+				"children ARRAY" +
+				");");
 
 		db.execute(
 			"CREATE TABLE readings (" +
