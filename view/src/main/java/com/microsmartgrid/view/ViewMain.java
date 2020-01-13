453ba90b9570e915c16cbd6ee95ef4df4795df75
Jannis Blossey <jannis@blossey.eu>
2020-01-13 20:01:49 +0100

    revert: make services independently executable

--- view/src/main/java/com/microsmartgrid/view/ViewMain.java
+++ view/src/main/java/com/microsmartgrid/view/ViewMain.java
@@ -1,7 +1,5 @@
 package com.microsmartgrid.view;
 
-import com.microsmartgrid.configserver.ConfigServerApplication;
-import com.microsmartgrid.eurekaserver.EurekaServerApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,9 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ViewMain {
 	public static void main(String[] args) {
-
-		SpringApplication.run(ConfigServerApplication.class, args);
-		SpringApplication.run(EurekaServerApplication.class, args);
 		SpringApplication.run(ViewMain.class, args);
 	}
 }
