0829e5eee9e30b383b42acf484ae1da73f21fd2e
dustin <19474420+Bytesizeweezy@users.noreply.github.com>
2020-01-13 19:39:44 +0100

    fix: remove java modules
    Spring has it's only management system and this was causing problems when building.

--- configserver/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.microsmartgrid.configserver {
-	requires spring.boot;
-	requires spring.cloud.config.server;
-	requires spring.boot.autoconfigure;
-	exports com.microsmartgrid.configserver;
-}
