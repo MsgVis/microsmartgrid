6d915d905b5b929de0de2d9d6e63c8bed15bdd5f
Lucca <lucca.gutleben@gmail.com>
2020-01-23 17:01:02 +0100

    refactor: queryFlow

--- database/src/main/java/com/microsmartgrid/database/dbCom/DbReader.java
+++ database/src/main/java/com/microsmartgrid/database/dbCom/DbReader.java
@@ -130,4 +130,163 @@ public class DbReader {
 
 		return info;
 	}
+
+	@GetMapping("/flow")
+	@ResponseBody
+	public static List<Readings> queryFlow(@RequestParam("start") Timestamp start, @RequestParam("end") Timestamp end) {
+		HashMap<String, Object> queryInfo = new HashMap<>();
+		queryInfo.put("aggregate", "last");
+		return generalReadingQuery(0, start, end, null, QUERY_READINGS_LAST, queryInfo);
+	}
+
+	@GetMapping("/readings")
+	@ResponseBody
+	public static List<Readings> queryAgg(@RequestParam("id") int id, @RequestParam("start") Timestamp start, @RequestParam("end") Timestamp end, @RequestParam("step") String step, @RequestParam("agg") String agg) {
+		HashMap<String, Object> queryInfo = new HashMap<>();
+		queryInfo.put("aggregate", agg);
+		queryInfo.put("interval", step);
+		if (agg.equals("avg")) {
+			return generalReadingQuery(id, start, end, step, QUERY_READINGS_AVERAGES, queryInfo);
+		} else if (agg.equals("std")) {
+			return generalReadingQuery(id, start, end, step, QUERY_READINGS_STDDEV, queryInfo);
+		} else if (agg.equals("min")) {
+			return generalReadingQuery(id, start, end, step, QUERY_READINGS_MIN, queryInfo);
+		} else if (agg.equals("max")) {
+			return generalReadingQuery(id, start, end, step, QUERY_READINGS_MAX, queryInfo);
+		} else {
+			logger.warn("Aggregate function " + agg + " is currently not supported");
+			return null;
+		}
+	}
+
+	public static List<Readings> queryReading(int id, Timestamp start, Timestamp end, String step) {
+		return generalReadingQuery(id, start, end, step, QUERY_READINGS, null);
+	}
+
+	public static List<Readings> queryMultiple(int id, Timestamp start, Timestamp end, String step, boolean standard, boolean avg) {
+		List<Readings> results = new ArrayList<>();
+		if (standard) results.addAll(queryReading(id, start, end, step));
+		if (avg) results.addAll(queryAgg(id, start, end, step, "avg"));
+		return results;
+	}
+
+	private static List<Readings> generalReadingQuery(int id, Timestamp start, Timestamp end, String step, String QUERY_FUNCTION, HashMap<String, Object> meta) {
+		List<Readings> readings = new ArrayList<>();
+		String DYNAMIC_QUERY = QUERY_READINGS_SELECT_START;
+		if (step == null && meta != null) {
+			DYNAMIC_QUERY += QUERY_READINGS_FLOW;
+		} else if (step != null) {
+			DYNAMIC_QUERY += QUERY_READINGS_BUCKET;
+		} else {
+			DYNAMIC_QUERY += QUERY_READINGS_TIME;
+		}
+		DYNAMIC_QUERY += QUERY_FUNCTION;
+
+		DYNAMIC_QUERY += FROM_READINGS;
+
+		boolean atleast_one = false;
+		DYNAMIC_QUERY += " WHERE";
+		if (id > 0) {
+			DYNAMIC_QUERY += FILTER_READINGS_ID;
+			atleast_one = true;
+		}
+		if (start != null) {
+			DYNAMIC_QUERY += FILTER_READINGS_TIME_AFTER;
+			atleast_one = true;
+		}
+		if (end != null) {
+			DYNAMIC_QUERY += FILTER_READINGS_TIME_BEFORE;
+			atleast_one = true;
+		}
+		if (atleast_one) {
+			DYNAMIC_QUERY = removeFromEnd(DYNAMIC_QUERY, " AND");
+		} else {
+			DYNAMIC_QUERY = removeFromEnd(DYNAMIC_QUERY, " WHERE");
+		}
+
+		if (step != null) DYNAMIC_QUERY += QUERY_READINGS_GROUP_BUCKET;
+		else DYNAMIC_QUERY += GROUP_BY_ID;
+		DYNAMIC_QUERY += ";";
+
+		try (Connection conn = getConnection();
+			 PreparedStatement stmt = conn.prepareStatement(DYNAMIC_QUERY)) {
+
+			int i = 1;
+			if (step != null) {
+				stmt.setInt(i++, Integer.parseInt(step));
+			}
+			if (id > 0) {
+				stmt.setInt(i++, id);
+			}
+			if (start != null) {
+				stmt.setTimestamp(i++, start);
+			}
+			if (end != null) {
+				stmt.setTimestamp(i++, end);
+			}
+
+			ResultSet rs = stmt.executeQuery();
+
+			if (rs == null) throw new SQLException("Database does not exist");
+
+			while (rs.next()) {
+				readings.add(createObject(rs, step, meta));
+			}
+
+			rs.close();
+		} catch (SQLException e) {
+			logger.warn("Could not fetch readings.");
+			e.printStackTrace();
+		}
+		return readings;
+	}
+
+	private static Readings createObject(ResultSet rs, String step, HashMap<String, Object> meta) {
+		try {
+			Readings read = new Readings(rs.getInt("device_id"), step != null ? rs.getTimestamp("bucket").toInstant() : rs.getTimestamp("time").toInstant());
+
+			read.setCurrent_I_avg(rs.getFloat("i_avg"));
+			read.setCurrent_I1(rs.getFloat("i_r"));
+			read.setCurrent_I2(rs.getFloat("i_s"));
+			read.setCurrent_I3(rs.getFloat("i_t"));
+			read.setVoltage_U_avg(rs.getFloat("u_avg"));
+			read.setVoltage_U1(rs.getFloat("u_r"));
+			read.setVoltage_U2(rs.getFloat("u_s"));
+			read.setVoltage_U3(rs.getFloat("u_t"));
+			read.setActive_energy_A_plus(rs.getFloat("a_plus"));
+			read.setActive_energy_A_minus(rs.getFloat("a_minus"));
+			read.setReactive_energy_R_plus(rs.getFloat("r_plus"));
+			read.setReactive_energy_R_minus(rs.getFloat("r_minus"));
+			read.setActive_power_P_total(rs.getFloat("p_total"));
+			read.setActive_power_P1(rs.getFloat("p_r"));
+			read.setActive_power_P2(rs.getFloat("p_s"));
+			read.setActive_power_P3(rs.getFloat("p_t"));
+			read.setReactive_power_Q_total(rs.getFloat("q_total"));
+			read.setReactive_power_Q1(rs.getFloat("q_r"));
+			read.setReactive_power_Q2(rs.getFloat("q_s"));
+			read.setReactive_power_Q3(rs.getFloat("q_t"));
+			read.setApparent_power_S_total(rs.getFloat("s_total"));
+			read.setApparent_power_S1(rs.getFloat("s_r"));
+			read.setApparent_power_S2(rs.getFloat("s_s"));
+			read.setApparent_power_S3(rs.getFloat("s_t"));
+			read.setFrequency_grid(rs.getFloat("f"));
+			if (step == null && meta == null) {
+				read.setMetaInformation(rs.getObject("meta", HashMap.class));
+			} else {
+				read.setMetaInformation(meta);
+			}
+			return read;
+		} catch (SQLException e) {
+			logger.warn("Could not create Reading object.");
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static String removeFromEnd(String DYNAMIC_QUERY, String toRemove) {
+		if (DYNAMIC_QUERY.endsWith(toRemove)) {
+			return DYNAMIC_QUERY.substring(0, DYNAMIC_QUERY.lastIndexOf(toRemove));
+		} else {
+			throw new RuntimeException();
+		}
+	}
 }
