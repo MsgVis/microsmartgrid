4c85cba51f6c1741df739212242c48ca66763043
Jannis Blossey <jannis@blossey.eu>
2020-01-28 14:45:27 +0100

    feat: separate DB Reader with refactored controllers and call paths

--- timescaleDbReader/src/main/java/com/microsmartgrid/timescaleDbReader/controller/TimescaleDbReaderController.java
+++ timescaleDbReader/src/main/java/com/microsmartgrid/timescaleDbReader/controller/TimescaleDbReaderController.java
@@ -1,17 +1,29 @@
-package com.microsmartgrid.timescaleDbReader.controller
+package com.microsmartgrid.timescaleDbReader.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.microsmartgrid.database.dbDataStructures.AdditionalDeviceInformation;
+import com.microsmartgrid.timescaleDbReader.TimescaleDbReaderApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/timescaleDbReader")
-public class DemoController {
+// TODO: Access noch richtig einstellen. Momentan kann jeder darauf zugreifen.
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class TimescaleDbReaderController {
 
-	@RequestMapping(path = "/subDemo", method = RequestMethod.GET)
-	public HelloObject getHelloWordObject() {
-		return "Hello Demo";
+	@RequestMapping(path = "/deviceList", method = RequestMethod.GET)
+	public List<AdditionalDeviceInformation> queryDeviceList() {
+		return TimescaleDbReaderApplication.queryDeviceList();
+	}
+
+	@RequestMapping(path = "/deviceById", method = RequestMethod.GET)
+	public AdditionalDeviceInformation queryDevices(@RequestParam("id") int id) {
+		return TimescaleDbReaderApplication.queryDevices(id);
+	}
+
+	@RequestMapping(path = "/deviceByName", method = RequestMethod.GET)
+	public AdditionalDeviceInformation queryDevices(@RequestParam("name") String name) {
+		return TimescaleDbReaderApplication.queryDevices(name);
 	}
 }
