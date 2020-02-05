d79155c1643892ba9fdb872a2ae3ae3dcc2a7c64
dustin <19474420+Bytesizeweezy@users.noreply.github.com>
2020-02-05 22:17:05 +0100

    fix: pass arguments to main method
    Remove redundant Test.

--- mqttclient/src/test/java/com/microsmartgrid/mqttclient/MqttClientApplicationTests.java
+++ mqttclient/src/test/java/com/microsmartgrid/mqttclient/MqttClientApplicationTests.java
@@ -1,21 +1,11 @@
 package com.microsmartgrid.mqttclient;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@SpringBootTest(args = {"tcp://mqtt.eclipse.org:1883", "bbc/subtitles/notice"})
 @ExtendWith(SpringExtension.class)
 class MqttClientApplicationTests {
 
-	@Autowired
-	private CommandLineRunner clr;
-
-	@Test
-	public void testMainRoutine() throws Exception {
-		this.clr.run("tcp://mqtt.eclipse.org:1883", "bbc/subtitles/notice");
-	}
 }
