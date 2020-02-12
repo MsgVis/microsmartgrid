24eb3c4b38bd1d728d0b8d35ab8f208c327c5662
dustin <19474420+Bytesizeweezy@users.noreply.github.com>
2020-02-12 01:17:54 +0100

    refactor: use AtomicReference instead of try catch
    remove uninformative/duplicate comments and console outputs.

--- database/src/main/java/com/microsmartgrid/database/HelperFunctions.java
+++ database/src/main/java/com/microsmartgrid/database/HelperFunctions.java
@@ -9,7 +9,7 @@ import lombok.extern.log4j.Log4j2;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Log4j2
 public class HelperFunctions {
@@ -20,33 +20,30 @@ public class HelperFunctions {
 	 */
 	public static Class<? extends AbstractDevice> getClassFromIdentifier(final String name) throws IOException {
 		Class<? extends AbstractDevice> cls;
-		String class_name;
+		AtomicReference<String> class_name = new AtomicReference<>();
 		final String package_start = "com.microsmartgrid.database.model.";
 
-		try {
-			ArrayList<LinkedHashMap<String, String>> classMaps = Configurations.retrieveClassMap();
-			class_name = package_start +
-				classMaps.stream()
-					.filter(t -> name.matches(t.get("regex")))
-					.map(t -> t.get("path"))
-					.findFirst().orElseThrow();
-		} catch (NoSuchElementException e) {
-			// No associated class for the name in ClassMaps.yml
-			log.warn(name + " could not be mapped to a class defined in ClassMap.yml;\n" +
-				"Defaulting to a standard class in order to save information in json.");
-			// use default class to save fields in 'meta_information'
-			class_name = package_start + "Device";
-		}
+
+		ArrayList<LinkedHashMap<String, String>> classMaps = Configurations.retrieveClassMap();
+		classMaps.stream()
+			.filter(t -> name.matches(t.get("regex")))
+			.map(t -> t.get("path"))
+			.findFirst().ifPresentOrElse(s -> class_name.set(package_start + s),
+			() -> {
+				// No associated class for the name in ClassMaps.yml
+				log.warn(name + " could not be mapped to a class defined in ClassMap.yml;\n" +
+					"Defaulting to a standard class in order to save information in json.");
+				class_name.set(package_start + "Device");
+			});
 
 		try {
-			cls = Class.forName(class_name).asSubclass(AbstractDevice.class);
+			cls = Class.forName(class_name.get()).asSubclass(AbstractDevice.class);
 		} catch (ClassNotFoundException ex) {
-			// Associated Class in ClassMaps.yml doesn't exist yet or there is a typo
-			log.warn(class_name + " is not yet implemented;\n" +
+			log.warn(class_name + " in ClassMaps.yml doesn't exist yet or there is a typo;\n" +
 				"Defaulting to a standard class in order to group information in json.");
-			// use default class to save fields in 'meta_information'
 			cls = Device.class;
 		}
+
 		log.debug("Using " + class_name + " class for topic " + name);
 		return cls;
 	}
@@ -58,7 +55,6 @@ public class HelperFunctions {
 
 		if (objMapper.canSerialize(cls) && json.startsWith("{")) {
 			// create object from json
-			log.debug("Deserializing json to class.");
 			device = objMapper.readValue(json, cls);
 		} else {
 			// TODO: figure out a way to handle jsonArrays and single attributes
