c9aa1d7c5daeaa8b66ac33b9699935d806570d06
dustin <19474420+Bytesizeweezy@users.noreply.github.com>
2020-02-12 01:22:05 +0100

    fix: controller not being included in base package scan

--- timescaleDbReader/src/main/java/com/microsmartgrid/timescaleDbReader/TimescaleDbReaderApplication.java
+++ timescaleDbReader/src/main/java/com/microsmartgrid/timescaleDbReader/TimescaleDbReaderApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.microsmartgrid.database"})
+@SpringBootApplication(scanBasePackages = {"com.microsmartgrid.database", "com.microsmartgrid.timescaleDbReader"})
 @EntityScan("com.microsmartgrid.database.model")
 @EnableJpaRepositories(basePackages = {"com.microsmartgrid.database"})
 @EnableDiscoveryClient
