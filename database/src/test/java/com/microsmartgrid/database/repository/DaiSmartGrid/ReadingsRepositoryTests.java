c48622bd1f86a9b971b7b3a4b33cc58407e6a61e
dustin <19474420+Bytesizeweezy@users.noreply.github.com>
2020-02-21 23:19:13 +0100

    test: Add some ReadingsRepository tests but...
    the h2 database cannot deal with the type definitions for json for example. Also we have no way of testing the timescale functions with h2.

---
+++ database/src/test/java/com/microsmartgrid/database/repository/DaiSmartGrid/ReadingsRepositoryTests.java
@@ -0,0 +1,85 @@
+package com.microsmartgrid.database.repository.DaiSmartGrid;
+
+import com.microsmartgrid.database.model.DaiSmartGrid.Readings;
+import com.microsmartgrid.database.model.DeviceInformation;
+import com.microsmartgrid.database.repository.DeviceInformationRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.Instant;
+import java.time.Period;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(properties = {"hibernate.dialect=org.hibernate.dialect.PostgreSQL95Dialect"})
+@Disabled("Doesn't work with h2 and custom type definitions")
+public class ReadingsRepositoryTests {
+
+	static Readings readings1 = new Readings();
+	static Readings readings2 = new Readings();
+	static DeviceInformation deviceInformation1 = new DeviceInformation("unique name");
+
+	@Autowired
+	private ReadingsRepository readingsRepository;
+	@Autowired
+	private DeviceInformationRepository deviceInformationRepository;
+
+	@BeforeAll
+	static void init() {
+		readings1.setDeviceInformation(deviceInformation1);
+		readings2.setDeviceInformation(deviceInformation1);
+
+		readings1.setTimestamp(Instant.now().minus(Period.ofDays(1)));
+		readings2.setTimestamp(Instant.now().minus(Period.ofDays(2)));
+	}
+
+	@Test
+	void testFindSingleWithoutCutoff() {
+		readingsRepository.save(readings1);
+		readingsRepository.save(readings2);
+		deviceInformationRepository.save(deviceInformation1);
+
+		assertThat(readingsRepository.findFirstByDeviceInformationAndTimestampAfterOrderByTimestampDesc(deviceInformation1, Instant.now().minus(Period.ofDays(3))))
+			.hasValue(readings1);
+	}
+
+	@Test
+	void testFindSingleWithCutoff() {
+		readingsRepository.save(readings1);
+		readingsRepository.save(readings2);
+		deviceInformationRepository.save(deviceInformation1);
+
+		assertThat(readingsRepository.findFirstByDeviceInformationAndTimestampAfterOrderByTimestampDesc(deviceInformation1, Instant.now().minus(Period.ofDays(1))))
+			.hasValue(readings1);
+	}
+
+	@Test
+	void testFindNoneWithCutoff() {
+		readingsRepository.save(readings1);
+		readingsRepository.save(readings2);
+		deviceInformationRepository.save(deviceInformation1);
+
+		assertThat(readingsRepository.findFirstByDeviceInformationAndTimestampAfterOrderByTimestampDesc(deviceInformation1, Instant.now().minus(Period.ofDays(0))))
+			.isEmpty();
+	}
+
+	@Test
+	void testFindNoneWithWrongDeviceId() {
+		readingsRepository.save(readings1);
+		readingsRepository.save(readings2);
+		DeviceInformation deviceInformation2 = new DeviceInformation("some test name");
+		deviceInformationRepository.save(deviceInformation2);
+
+		assertThat(readingsRepository.findFirstByDeviceInformationAndTimestampAfterOrderByTimestampDesc(deviceInformation2, Instant.now().minus(Period.ofDays(3))))
+			.isEmpty();
+	}
+
+	@Test
+	@Disabled("Can't test without timescaledb")
+	void testFindReadings() {
+
+	}
+}
