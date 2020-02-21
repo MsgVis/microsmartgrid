97db46751a2cb9d2b214c27c75f25c90c257259d
dustin <19474420+Bytesizeweezy@users.noreply.github.com>
2020-02-22 00:38:10 +0100

    test: add devicesBy tests

--- timescaleDbReader/src/test/java/com/microsmartgrid/timescaleDbReader/controller/TimescaleDbReaderControllerTests.java
+++ timescaleDbReader/src/test/java/com/microsmartgrid/timescaleDbReader/controller/TimescaleDbReaderControllerTests.java
@@ -7,22 +7,22 @@ import com.microsmartgrid.database.repository.DeviceInformationRepository;
 import com.microsmartgrid.database.service.DaiSmartGrid.ReadingsService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest(TimescaleDbReaderController.class)
 public class TimescaleDbReaderControllerTests {
 
@@ -64,13 +64,41 @@ public class TimescaleDbReaderControllerTests {
 			.andExpect(status().isOk())
 			.andExpect(mvcResult ->
 				assertThat((List<?>) objM.readValue(mvcResult.getResponse().getContentAsString(),
-					objM.getTypeFactory().constructCollectionType(List.class, DeviceInformation.class))).isEqualTo(devices)
+					objM.getTypeFactory().constructCollectionType(List.class, DeviceInformation.class)))
+					.isEqualTo(devices)
 			);
 	}
 
 	@Test
-	public void testDevicesById() {
-		//when(deviceRepo.findAllById(anyInt())).thenReturn(devices.get(anyInt()));
+	public void testDevicesByIdPositive() throws Exception {
+		when(deviceRepo.findById(anyInt())).thenAnswer(i -> Optional.of(devices.get((Integer) i.getArguments()[0])));
+
+		mvc.perform(get("/deviceById").param("id", "0"))
+			.andExpect(status().isOk())
+			.andExpect(mvcResult ->
+				assertThat(objM.readValue(mvcResult.getResponse().getContentAsString(), DeviceInformation.class))
+					.isEqualTo(device1)
+			);
+	}
+
+	@Test
+	public void testDevicesByIdNegative() throws Exception {
+		when(deviceRepo.findById(anyInt())).thenReturn(Optional.empty());
+
+		mvc.perform(get("/deviceById").param("id", "3"))
+			.andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void testDevicesByNameNegative() throws Exception {
+		when(deviceRepo.findByName(anyString())).thenReturn(Optional.empty());
+
+		mvc.perform(get("/deviceByName").param("name", "unique"))
+			.andExpect(status().isOk())
+			.andExpect(mvcResult ->
+				assertThat(mvcResult.getResponse().getContentType())
+					.isEqualTo(null)
+			);
 	}
 
 
